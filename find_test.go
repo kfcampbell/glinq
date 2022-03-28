6398ad318afcdf4a0ad3670a70da655e831b2ba3
Keegan Campbell <me@kfcampbell.com>
2022-03-28 15:35:49 -0700

    Test IndexOf and LastIndexOf

--- find_test.go
+++ find_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestIndexOfInt(t *testing.T) {
 	cases := []struct {
-		info     string
+		name     string
 		input    []int
 		elem     int
 		expected int
@@ -35,12 +35,129 @@ func TestIndexOfInt(t *testing.T) {
 			9,
 			1,
 		},
+		{
+			"doesNotExist",
+			[]int{8, 9, 10, 11, 12},
+			14,
+			-1,
+		},
 	}
 
 	for _, tc := range cases {
 		actual := IndexOf(tc.input, tc.elem)
 		if actual != tc.expected {
-			t.Errorf("%v: expected %v, got %v", tc.info, tc.elem, actual)
+			t.Errorf("TestIndexOfInt %v: expected %v, got %v", tc.name, tc.elem, actual)
+		}
+	}
+}
+
+func TestIndexOfString(t *testing.T) {
+	cases := []struct {
+		name     string
+		input    []string
+		elem     string
+		expected int
+	}{
+		{
+			"happyCase",
+			[]string{"abc", "def", "hijk"},
+			"def",
+			1,
+		},
+		{
+			"doesNotExist",
+			[]string{"abc", "def", "hijk"},
+			"lmnop",
+			-1,
+		},
+	}
+
+	for _, tc := range cases {
+		actual := IndexOf(tc.input, tc.elem)
+		if actual != tc.expected {
+			t.Errorf("TestIndexOfString %v: expected %v, got %v", tc.name, tc.elem, actual)
+		}
+	}
+}
+
+func TestLastIndexOfInt(t *testing.T) {
+	cases := []struct {
+		name     string
+		input    []int
+		elem     int
+		expected int
+	}{
+		{
+			"happyCase",
+			[]int{1, 2, 3},
+			2,
+			1,
+		},
+		{
+			"twice",
+			[]int{1, 2, 3, 4, 5, 4},
+			4,
+			5,
+		},
+		{
+			"thrice",
+			[]int{1, 2, 2, 2, 4},
+			2,
+			3,
+		},
+		{
+			"doesNotExist",
+			[]int{4, 18, 19, 22},
+			42,
+			-1,
+		},
+	}
+
+	for _, tc := range cases {
+		actual := LastIndexOf(tc.input, tc.elem)
+		if actual != tc.expected {
+			t.Errorf("TestLastIndexOfInt %v: expected %v, got %v", tc.name, tc.elem, actual)
+		}
+	}
+}
+
+func TestLastIndexOfString(t *testing.T) {
+	cases := []struct {
+		name     string
+		input    []string
+		elem     string
+		expected int
+	}{
+		{
+			"happyCase",
+			[]string{"abc", "def", "hijk"},
+			"def",
+			1,
+		},
+		{
+			"twice",
+			[]string{"abc", "def", "def", "hijk"},
+			"def",
+			2,
+		},
+		{
+			"thrice",
+			[]string{"abc", "def", "def", "def", "hijk"},
+			"def",
+			3,
+		},
+		{
+			"doesNotExist",
+			[]string{"abc", "def", "hijk"},
+			"lmnop",
+			-1,
+		},
+	}
+
+	for _, tc := range cases {
+		actual := LastIndexOf(tc.input, tc.elem)
+		if actual != tc.expected {
+			t.Errorf("TestLastIndexOfString %v: expected %v, got %v", tc.name, tc.expected, actual)
 		}
 	}
 }
